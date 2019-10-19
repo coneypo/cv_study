de828d2f342127ab472946459edd1c8ffda9880a
coneypo <coneypo@gmail.com>
2019-10-19 10:06:21 +0800

    update rendering cube

--- cv2_pose_est_cube_camera.py
+++ cv2_pose_est_cube_camera.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 import glob
 
-
 # refer to https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_pose/py_pose.html#pose-estimation
 
 # termination criteria
@@ -11,14 +10,14 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(5,5,0)
 objp = np.zeros((5 * 5, 3), np.float32)
 objp[:, :2] = np.mgrid[0:5, 0:5].T.reshape(-1, 2)
-axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
-                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],
+                   [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # cap.set(propId, value)
 cap.set(3, 480)
@@ -28,7 +27,15 @@ def draw(img, corners, imgpts):
     imgpts = np.int32(imgpts).reshape(-1, 2)
 
     # draw ground floor in green
-    img = cv2.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+    # img = cv2.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+
+    # print(imgpts)
+    font = cv2.FONT_HERSHEY_COMPLEX
+    cv2.putText(img_camera, "Landing here 0", (int(imgpts[0][0]), int(imgpts[0][1])), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.putText(img_camera, "Landing here 1", (int(imgpts[1][0]), int(imgpts[1][1])), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.putText(img_camera, "Landing here 2", (int(imgpts[2][0]), int(imgpts[2][1])), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    # cv2.putText(img_camera, "Landing here 01", (int(imgpts[2][0]), int(imgpts[2][1])), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
     # draw pillars in blue color
     for i, j in zip(range(4), range(4, 8)):
@@ -93,7 +100,7 @@ while cap.isOpened():
         imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
         img_camera = draw(img_camera, corners2, imgpts)
     except:
-        print("no 3D")
+        pass
 
     cv2.imshow('img', img_camera)
 
